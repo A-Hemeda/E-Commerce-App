b4f5e574d899ea100529a593ead64eb9495eb37d
Ahmed Hemeda <101954795+A-Hemeda@users.noreply.github.com>
2025-01-11 16:50:27 +0200

    Delete lib directory

--- lib/modules/shop_app/categorey/categorey_screen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class CategoriesScreen extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Container(child: Text('Categorey')));
-  }
-}
--- lib/shared/network/end_point.dart
+++
@@ -1,3 +0,0 @@
-const Login = 'login';
-
-const HOME = 'home';
